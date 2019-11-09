3c8950d725dde8df6d2d8f151819e3b46dfcc367
Cara Steffen <cara@treadstonedesign.com>
2019-11-09 09:51:11 -0700

    deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   //API_ENDPOINT: "http://localhost:9090"
-  API_ENDPOINT: "https://stormy-garden-68077.herokuapp.com/"
+  API_ENDPOINT: "https://stormy-garden-68077.herokuapp.com"
   //API_ENDPOINT: "postgresql://postgres@localhost/noteful2-test"
 };
