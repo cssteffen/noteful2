ffd473e31d22d4f5990d548212209f994872a92e
Cara Steffen <cara@treadstonedesign.com>
2019-11-09 18:27:37 -0700

    displaying content

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,12 +1,14 @@
 export const findFolder = (folders = [], folder_id) =>
-  folders.find(folder => folder.id === folder_id);
+  folders.find(folder => parseInt(folder.id) === parseInt(folder_id));
 
 export const findNote = (notes = [], noteId) =>
   //notes.find(note => note.id === noteId);
   notes.find(note => parseInt(note.id) === parseInt(noteId));
 
 export const getNotesForFolder = (notes = [], folder_id) =>
-  !folder_id ? notes : notes.filter(note => note.folder_id === folder_id);
+  !folder_id
+    ? notes
+    : notes.filter(note => note.folder_id === parseInt(folder_id));
 
 export const countNotesForFolder = (notes = [], folder_id) =>
-  notes.filter(note => note.folder_id === folder_id).length;
+  notes.filter(note => note.folder_id === parsInt(folder_id)).length;
